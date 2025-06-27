20cee0235dc882c2a9e0991c725b042677241bc7
devarenidhi25 <devarenidhi@gmail.com>
2025-06-27 12:08:49 +0530

    update footer.js

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -88,7 +88,7 @@ const Footer = () => {
         <div className="footer-section info-section">
           <h3>Quick Links</h3>
           <ul className="footer-links">
-            <li><a href="#home">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="/predictor">Disease Predictor</a></li>
             <li><a href="/report-scanner">Report Scanner</a></li>
